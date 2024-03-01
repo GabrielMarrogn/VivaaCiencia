3ef879f239ab20c5286da9710eb249364c0bb209
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-03-01 12:36:23 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        sc.nextLine();
+
+        System.out.println("Nao se refuta ciencia com opiniao");
+
+        sc.close();
+        
+    }
+}
